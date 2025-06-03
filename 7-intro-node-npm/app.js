717c79f423a5c9f5f4c0370b43a295000a7943ca
Diana Amillano <estephanirobles23@gmail.com>
2025-06-03 04:24:44 -0600

    intro-node-npm

---
+++ 7-intro-node-npm/app.js
@@ -0,0 +1,12 @@
+
+// NPM
+/** NPM son las siglas de Node Package Manager, es decir, gestor de paquetes de Node.js.
+ * Este gestor de paquetes, nos permité instalar de forma muy sencilla y automática paquetes Javascript.
+ * Estos paquetes nos brindas superpoderes para realizar tareas muy específicas sin que nosotros tengamos que hacer
+ * su programación de 0
+ * Al instalar Node.js este instalará también consigo una serie de utilidades, incluidos su gestor de paquetes: npm.*/
+//package.json
+/**
+ * El proyecto debe contener un archivo llamado package.json. 
+ * Dentro de ese archivo, encontarás metadatos específicos para los proyectos, como nombre del proyecto, autor, versión
+ * licencia, etc.*/
